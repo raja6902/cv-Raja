a2458b3757cdcd58551326901dfcff616d4bb849
Raja Khan <rajashahidullah@hotmail.com>
2020-11-25 01:14:48 +0100

    Commit

--- doc/javas.js
+++ doc/javas.js
@@ -1,6 +1,7 @@
 function myFunction() {
     var x = document.getElementById("myDIV");
-    if (x.style.display === "none") {
+
+    if (x.style.display === "none"|| x.style.display==""){
         x.style.display = "block";
     } else {
         x.style.display = "none";
